91ed909e069793b5141c950e47c247ac5793378b
ShurikFW <aleksandrmastakov2001@gmail.com>
2024-04-25 01:20:26 +0300

    Changing homepage button and text design

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -40,33 +40,81 @@ class _MainPageState extends State<MainPage> {
     ));
   }
 
-  @override
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Estonian Learning App Bar'),
+        title: Text(
+          'Estonian Learning App ',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             _userLevel != null
-                ? Text('User Level: $_userLevel / 20')
+                ? Text(
+                    'User Level: $_userLevel / 20',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  )
                 : CircularProgressIndicator(),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _incrementLevel,
-              child: Text('Increment Level'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _clearLevel,
-              child: Text('Clear Level'),
-            ),
-            SizedBox(height: 20),
+            SizedBox(height: 40),
             ElevatedButton(
               onPressed: _navigateToLevel,
-              child: Text('Start'),
+              child: Text(
+                'Start game',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
+                shape: MaterialStateProperty.all<OutlinedBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+              ),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: _incrementLevel,
+              child: Text(
+                'Increment Level',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
+                shape: MaterialStateProperty.all<OutlinedBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+              ),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: _clearLevel,
+              child: Text(
+                'Clear Level',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 40, vertical: 20)),
+                shape: MaterialStateProperty.all<OutlinedBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30))),
+              ),
             ),
           ],
         ),
